65817a76a662ab010d7a1887313bc2b91e8d2597
Csaba <cstolcser@gmail.com>
2018-05-24 23:14:11 +0200

    Added integration test

---
+++ consoleapp/src/test/java/employee/ConsoleApplicationTest.java
@@ -0,0 +1,19 @@
+package com.employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsoleApplicationTest {
+
+    //@Rule
+    //public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
